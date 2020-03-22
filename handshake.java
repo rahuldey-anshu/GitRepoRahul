6863dc8b623eea288a9e52075933fd4fa09b0f55
Rahul Dey <rd90963@gmail.com>
2020-03-22 18:03:23 +0530

    Handshake program initial commit

---
+++ handshake.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Handshakes {
+
+   public static void main (String[] args)
+   	 {
+   	 	Scanner sc=new Scanner(System.in);
+   	 	int n,total;
+   	 	System.out.println ("Enter the no of people");
+   	 	n=sc.nextInt();
+   	 	total=n*(n-1)/2;
+   	 	System.out.println ("total no of handshakes" +  " = " + total+ " ");
+   	 	
+}
+    
+    
+}
